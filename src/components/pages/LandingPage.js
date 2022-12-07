d26ef1405cc67515615496839c4e4de456c8c3ce
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-07 09:05:49 +0300

    feature: added content to landing-page

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,7 +1,10 @@
 export default function LandingPage(){
     return(
-    <>
-    <h1>GOOD-WILL<br/>Cargo</h1>
-    </>
+    <div className="landing-page">
+        <span>Login</span>
+        <h1 className="company-name">GOOD-WILL<br/>Cargo</h1>
+        <div className="landing-page-overlay"></div>
+        <div className="landing-page-img"></div>
+    </div>
     );
 }
