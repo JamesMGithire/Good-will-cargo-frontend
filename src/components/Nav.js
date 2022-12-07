2fd3467e9e5a6817fedae1d140f65b6d7ca71acb
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-07 09:08:31 +0300

    feature: added Account and Ships Links to Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,11 @@
+import { NavLink } from "react-router-dom";
+
 export default function Nav(){
+    
     return(
-    <></>
+    <nav>
+        <NavLink>Account</NavLink>
+        <NavLink>Ships</NavLink>
+    </nav>
     )
 }
