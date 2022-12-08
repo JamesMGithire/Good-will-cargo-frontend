7c997ee41ddb300167776ecb409086cea3b95c5f
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-09 00:48:27 +0300

    testing useref

--- src/components/pages/Booking.js
+++ src/components/pages/Booking.js
@@ -3,11 +3,11 @@ import { useRef } from "react"
 export default function Booking({form}) {
   const bookingDiv = useRef();
   function closeBooking(){
-    console.log(bookingDiv.current.parentNode)
+    console.log(bookingDiv.current.parentNode.classList.value)
   }
   return (
     <>
-      <div ref={bookingDiv} className="booking-cover-div"></div>
+      <div ref={bookingDiv} className="booking-cover-div">Close</div>
       <div className="booking-div">
         <div onClick={closeBooking} className="close-booking-div"></div>
         {form}
