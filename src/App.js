e9343d3bf50c20ffeb3c2d704f645aec9b714cd9
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-12 00:20:36 +0300

    added custom cursor and functionality

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './styling/App.css'
 import Nav from './components/Nav'
 import { useLoggedInContext } from './context/LoggedIn'
 import Body from './components/Body'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 function App() {
   const { setLoggedIn } = useLoggedInContext()
+  const customCursor = useRef()
+  const moveCursor = (e) => {
+    const mouseY = e.clientY
+    const mouseX = e.clientX
+
+    customCursor.current.style.transform = `translate3d(${mouseX - 20}px, ${
+      mouseY - 20
+    }px, 0)`
+  }
+
+  window.addEventListener('mousemove', moveCursor)
   useEffect(() => {
     fetch('https://good-will-cargo-spark-production.up.railway.app/me', {
       headers: { Authorization: `Bearer ${localStorage.getItem('jwt')}` },
@@ -13,9 +24,14 @@ function App() {
       if (r.ok) {
         r.json().then((loggedUser) => {
           setLoggedIn(() => ({ user: { ...loggedUser } }))
-          fetch('https://good-will-cargo-spark-production.up.railway.app/user_cargos', {
-            headers: { Authorization: `Bearer ${localStorage.getItem('jwt')}` },
-          })
+          fetch(
+            'https://good-will-cargo-spark-production.up.railway.app/user_cargos',
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+              },
+            },
+          )
             .then((r) => r.json())
             .then((cargos) => {
               setLoggedIn((prevData) => ({
@@ -28,10 +44,15 @@ function App() {
     // eslint-disable-next-line
   }, [])
   return (
-    <div className="App">
-      <Nav />
-      <Body />
-    </div>
+    <>
+      <div ref={customCursor} className="custom-cursor">
+        <div></div>
+      </div>
+      <div className="App">
+        <Nav />
+        <Body />
+      </div>
+    </>
   )
 }
 
